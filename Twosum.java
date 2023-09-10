5c481c6720b21f4a4d0c9bf415da4f2b0a3b3b83
Maniprabha S <prabha6603@gmail.com>
2023-09-10 21:15:11 +0530

    updated

--- Twosum.java
+++ Twosum.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+class Main{
+    public int[] TwoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++)
         {
             for(int j=i+1;j<nums.length;j++)
