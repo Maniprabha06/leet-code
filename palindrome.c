f870154448a3c6825661872ce7afdf69780bf281
Maniprabha S <prabha6603@gmail.com>
2023-09-11 21:28:26 +0530

    updated

---
+++ palindrome.c
@@ -0,0 +1,30 @@
+int isPalindrome(int x){
+    if(x<0 || x!=0 && x%10==0)
+    {
+        return "false";
+    }
+    if(x/10==0)
+    {
+        return "true";
+    }
+    long int digit,new=0,temp;
+    temp=x;
+    while(temp>0)
+    {
+        digit=temp%10;
+        new=new*10+digit;
+        temp=temp/10;
+        if((new==temp || (x/10)==new))
+        {
+            return "true";
+        }
+    }
+    if(x==new)
+    {
+        return "true";
+    }
+    else
+    {
+        return "false";
+    }
+}
