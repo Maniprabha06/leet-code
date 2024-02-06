ae9fb8ae9d702f7b0b42258f4d1b57286ebb0521
Maniprabha S <108254371+Maniprabha06@users.noreply.github.com>
2024-02-06 23:49:39 +0530

    Update 1227. Airplane Seat Assignment Probability.java

--- 1227. Airplane Seat Assignment Probability.java
+++ 1227. Airplane Seat Assignment Probability.java
@@ -1,6 +1,9 @@
 class Solution {
     public double nthPersonGetsNthSeat(int n) {
-        if(n==1) return n;
+        if(n==1) 
+        {
+            return n;
+        }
         return 0.5;
     }
 }
